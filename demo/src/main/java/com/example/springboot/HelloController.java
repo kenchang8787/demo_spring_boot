0c42a49a8cb59e5d1732846b9e192d7dc62bddf4
Ken Chang <g-kenchang@gashpoint.com>
2022-07-20 10:18:23 +0800

    Formattor

--- demo/src/main/java/com/example/springboot/HelloController.java
+++ demo/src/main/java/com/example/springboot/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String Index() {
-        return "Greeting from Spring Boot!";
-    }
+  @GetMapping("/")
+  public String Index() {
+    return "Greeting from Spring Boot!";
+  }
 }
